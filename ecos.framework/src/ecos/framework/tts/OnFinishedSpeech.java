01f5cefa599cd81f0457e4e82602ef1d022f0e03
Pablo C. Vergara <pablocristobal@gmail.com>
2012-04-09 22:23:35 +0200

    Elimino temporalmente el código de tts

--- ecos.framework/src/ecos/framework/tts/OnFinishedSpeech.java
+++
@@ -1,5 +0,0 @@
-package ecos.framework.tts;
-
-public interface OnFinishedSpeech {
-    void speechFinished();
-}
--- ecos.framework/src/ecos/framework/tts/TtsEngine.java
+++
@@ -1,6 +0,0 @@
-package ecos.framework.tts;
-
-public interface TtsEngine {
-    void setOnFinishedSpeech(OnFinishedSpeech onFinishedSpeech);
-    void speak(String textToSpeak);
-}
