18fab6dc56814d238d01b14976379dd8d67305db
Pablo C. Vergara <pablocristobal@gmail.com>
2012-07-28 23:43:57 +0200

    Formateo

--- ecos.kidsgame.domainlogic/src/ecos/kidsgame/domainlogin/SeleccionarLasSilabasIndicadas.java
+++ ecos.kidsgame.domainlogic/src/ecos/kidsgame/domainlogin/SeleccionarLasSilabasIndicadas.java
@@ -5,51 +5,51 @@ import java.util.Collection;
 
 public class SeleccionarLasSilabasIndicadas {
 
-    private EstadoDeLaPrueba mEstado;
-    private ArrayList<Silaba> mSilabas;
-    private Silaba mSilaba;
+	private EstadoDeLaPrueba mEstado;
+	private ArrayList<Silaba> mSilabas;
+	private Silaba mSilaba;
 
-    public SeleccionarLasSilabasIndicadas(Collection<Silaba> silabas) {
-	mEstado = EstadoDeLaPrueba.Inicial;
-	mSilabas = new ArrayList<Silaba>(silabas);
-    }
-
-    public String getExplicacion() {
-	mEstado = EstadoDeLaPrueba.PendienteMasInformacion;
-	return "En esta prueba irás escuchando sílabas y tendrás que indicar qué sílaba suena cada vez";
-    }
-
-    public EstadoDeLaPrueba getEstado() {
-	return mEstado;
-    }
-
-    public Silaba getSilabaPendiente() {
-	mSilaba = null;
-	if (mSilabas.size() > 0) {
-	    mSilaba = mSilabas.get(0);
-	    mSilabas.remove(0);
+	public SeleccionarLasSilabasIndicadas(Collection<Silaba> silabas) {
+		mEstado = EstadoDeLaPrueba.Inicial;
+		mSilabas = new ArrayList<Silaba>(silabas);
 	}
 
-	return mSilaba;
-    }
-
-    public boolean jugar(Silaba silaba) {
-
-	if (mSilaba == null)
-	    return false;
-
-	boolean resultadoCorrecto = mSilaba.equals(silaba);
-	if (mEstado != EstadoDeLaPrueba.Finalizada) {
-	    if (resultadoCorrecto) {
+	public String getExplicacion() {
 		mEstado = EstadoDeLaPrueba.PendienteMasInformacion;
-		if (mSilabas.size() == 0)
-		    mEstado = EstadoDeLaPrueba.Finalizada;
-	    } else {
-		mEstado = EstadoDeLaPrueba.EsperandoRespuesta;
-	    }
+		return "En esta prueba irás escuchando sílabas y tendrás que indicar qué sílaba suena cada vez";
 	}
-	return resultadoCorrecto;
 
-    }
+	public EstadoDeLaPrueba getEstado() {
+		return mEstado;
+	}
+
+	public Silaba getSilabaPendiente() {
+		mSilaba = null;
+		if (mSilabas.size() > 0) {
+			mSilaba = mSilabas.get(0);
+			mSilabas.remove(0);
+		}
+
+		return mSilaba;
+	}
+
+	public boolean jugar(Silaba silaba) {
+
+		if (mSilaba == null)
+			return false;
+
+		boolean resultadoCorrecto = mSilaba.equals(silaba);
+		if (mEstado != EstadoDeLaPrueba.Finalizada) {
+			if (resultadoCorrecto) {
+				mEstado = EstadoDeLaPrueba.PendienteMasInformacion;
+				if (mSilabas.size() == 0)
+					mEstado = EstadoDeLaPrueba.Finalizada;
+			} else {
+				mEstado = EstadoDeLaPrueba.EsperandoRespuesta;
+			}
+		}
+		return resultadoCorrecto;
+
+	}
 
 }
