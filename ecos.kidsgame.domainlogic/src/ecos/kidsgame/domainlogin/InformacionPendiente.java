841241eefa2dd334eb31e60c94195f02c56769a5
Pablo C. Vergara <pablocristobal@gmail.com>
2012-08-12 13:41:39 +0200

    Agrego la palabra incompleta a información pendiente (en principio los test no lo requieren ... por eso que no aparecía, pero se supone que es lo que se utilizará para poder jugar a la tercera prueba)

--- ecos.kidsgame.domainlogic/src/ecos/kidsgame/domainlogin/InformacionPendiente.java
+++ ecos.kidsgame.domainlogic/src/ecos/kidsgame/domainlogin/InformacionPendiente.java
@@ -2,20 +2,25 @@ package ecos.kidsgame.domainlogin;
 
 public class InformacionPendiente {
 
-    private Silaba mSilaba;
-    private Palabra mPalabraCompleta;
+	private Silaba mSilaba;
+	private Palabra mPalabraCompleta;
+	private Palabra mPalabraIncompleta;
 
-    public InformacionPendiente(Palabra palabraIncompleta, Palabra palabraCompleta, Silaba silabaQueCompletaLaPalabra) {
-	mSilaba = silabaQueCompletaLaPalabra;
-	mPalabraCompleta = palabraCompleta;
-    }
+	public InformacionPendiente(Palabra palabraIncompleta, Palabra palabraCompleta, Silaba silabaQueCompletaLaPalabra) {
+		mPalabraIncompleta = palabraIncompleta;
+		mSilaba = silabaQueCompletaLaPalabra;
+		mPalabraCompleta = palabraCompleta;
+	}
 
-    public Silaba getSilaba() {
-	return mSilaba;
-    }
+	public Silaba getSilaba() {
+		return mSilaba;
+	}
 
-    public Palabra getPalabraCompleta() {
-	return mPalabraCompleta;
-    }
+	public Palabra getPalabraCompleta() {
+		return mPalabraCompleta;
+	}
 
+	public Palabra getPalabraIncompleta() {
+		return mPalabraIncompleta;
+	}
 }
